3d4745cdc4aa3d02b8e0f2fe3a1ad5331bdc5a82
yunsheng183 <yunshenz@unc.edu>
2026-02-25 14:26:41 -0500

    Submit EX03 to GitHub

---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,67 @@
+__author__ = "730947912"
+
+WHITE_BOX: str = "\U00002b1c"
+GREEN_BOX: str = "\U0001f7e9"
+YELLOW_BOX: str = "\U0001f7e8"
+
+
+def input_guess(secret_word_len: int) -> str:
+    """To guess the correct length."""
+    guess: str = input(f"Enter a {secret_word_len} character word: ")
+
+    while len(guess) != secret_word_len:
+        guess = input(f"That wasn't {secret_word_len} chars! Try again: ")
+
+    return guess
+
+
+def contains_char(secret_word: str, char_guess: str) -> bool:
+    """Guess the character founds in secret word."""
+    assert len(char_guess) == 1
+    idx: int = 0
+    while idx < len(secret_word):
+        if secret_word[idx] == char_guess:
+            return True
+        idx += 1
+    return False
+
+
+def emojified(guess: str, secret: str) -> str:
+    """Emoji string comparing guess to secret word."""
+    assert len(guess) == len(secret)
+    result: str = ""
+    idx: int = 0
+    while idx < len(secret):
+        if guess[idx] == secret[idx]:
+            result += GREEN_BOX
+        elif contains_char(secret, guess[idx]):
+            result += YELLOW_BOX
+        else:
+            result += WHITE_BOX
+        idx += 1
+    return result
+
+
+def main(secret: str) -> None:
+    """The entrypoint and mian loop."""
+    turn: int = 1
+    max_turns: int = 6
+    won: bool = False
+
+    while turn <= max_turns and not won:
+        print(f"=== Turn{turn}/{max_turns}===")
+        guess: str = input_guess(len(secret))
+        emoji_result: str = emjified(guess, secret)
+        print(emoji_result)
+        if guess == secret:
+            won = True
+        else:
+            turn += 1
+    if won:
+        print(f"You won in {turn}/{max_turns} turns!")
+    else:
+        print("X/6 - Sorry, try again tomorrow!")
+
+
+if __name__ == "__main__":
+    main(secret="codes")
---
+++ lessons/lists.py
@@ -0,0 +1,18 @@
+my_numbers: list[float] = []
+
+my_numbers.append(1, 5)
+
+print(my_numbers)
+
+
+game_points: list[int] = [107, 86, 94]
+
+print(game_points[2])
+
+game_points[1] = 72
+
+print(len(game_points))
+
+game_points.pop(1)
+
+print(game_points)
